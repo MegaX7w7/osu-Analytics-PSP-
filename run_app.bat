5220547e5cb714d1c326ce89677e4d4b4787fc20
Mega <mega.torres2314@gmail.com>
2026-06-09 20:34:47 -0500

    bat changes

--- run_app.bat
+++ run_app.bat
@@ -2,5 +2,8 @@
 echo Iniciando Osu!Analytics
 call .venv\Scripts\activate.bat
 echo "Running..."
+echo "running localhost:5000"
+start http://localhost:5000
+echo "Server Start."
 python src\app.py
 pause
